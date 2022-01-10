6447b180dc65e3caf9e8dee2bcaef0cad466ed30
LE TAN LOC <letanlocjp1001@gmail.com>
2022-01-10 14:43:35 +0900

    update

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,28 +1,37 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button, Platform } from 'react-native'
+import { StyleSheet, Text, View, FlatList, Platform } from 'react-native'
 
-import { CATEGORIES } from '../data/dummy-data'
-import Colors from '../constants/Colors'
-
+import { CATEGORIES, MEALS } from '../data/dummy-data'
+import MealItem from '../components/MealItem'
 const CategoryMealsScreen = (props) => {
-  const catId = props.navigation.getParam('categoryId')
-  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId)
-
-  return (
-    <View style={styles.screen}>
-      <Text>THE CATEGORY MEALS SCREEN</Text>
-      <Text>{selectedCategory.title}</Text>
-      <Button
-        title='Go to Details'
-        onPress={() => {
+  const renderMealIteam = (itemData) => {
+    return (
+      <MealItem
+        title={itemData.item.title}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affirdability={itemData.item.affirdability}
+        image={itemData.item.imageUrl}
+        onSelectMeal={() => {
           props.navigation.navigate({ routeName: 'MealDetail' })
         }}
       />
-      <Button
-        title='Go Back'
-        onPress={() => {
-          props.navigation.pop()
-        }}
+    )
+  }
+
+  const catId = props.navigation.getParam('categoryId')
+
+  const displayedMeals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(catId) >= 0
+  )
+
+  return (
+    <View style={styles.screen}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealIteam}
+        style={{ width: '100%' }}
       />
     </View>
   )
